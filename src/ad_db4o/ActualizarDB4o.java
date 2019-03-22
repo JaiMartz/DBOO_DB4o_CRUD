6e311bec0963a2052c8801f2ab34d5be2ec8a7cf
JaiMartz <martinezgarridojairo@gmail.com>
2019-03-22 23:23:05 +0000

    ActualizarDB4o is now able to update the DB contain.

--- src/ad_db4o/ActualizarDB4o.java
+++ src/ad_db4o/ActualizarDB4o.java
@@ -5,10 +5,40 @@
  */
 package ad_db4o;
 
+import com.db4o.*;
+import com.db4o.query.Query;
+
 /**
  *
- * @author Yleve
+ * @author Jairo
  */
 public class ActualizarDB4o {
+    public static void main(String[] args) {
+        //PATH base de datos
+        String pathBD = "C:\\Users\\Yleve\\Desktop\\BDOO\\gestionLaboral1819.yap";
+        
+        //Establecemos conexion con la BD
+        ObjectContainer db = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(),pathBD);
     
+        //Preparamos la consulta
+        Query consulta = db.query();
+        //Establecemos la consulta a traves del campo salario, indicamos que deben ganar menos de 1500
+        consulta.descend("salario").constrain(1500).smaller();
+        //Guardamos el resultado de la consulta en una lista
+        ObjectSet resultado = consulta.execute();
+        
+        if(resultado.size() == 0){
+            System.out.println("No existen empleados con los datos especificados.");
+        }else{
+            //Recorremos y modificamos el empleado que cumple las condiciones
+            while(resultado.hasNext()){
+                Empleado emp_actual = (Empleado)resultado.next();
+                emp_actual.setSalario(emp_actual.getSalario()+100);
+                db.store(emp_actual);
+            }
+        }
+        //Cerramos
+        db.close();
+    }
+
 }
