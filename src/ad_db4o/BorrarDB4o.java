135ddacc9d7c18e97fe486170464fdc727e6a3c2
JaiMartz <martinezgarridojairo@gmail.com>
2019-03-22 23:44:36 +0000

    BorrarDB4o now is able to delete an Employee from the DB.

--- src/ad_db4o/BorrarDB4o.java
+++ src/ad_db4o/BorrarDB4o.java
@@ -5,10 +5,39 @@
  */
 package ad_db4o;
 
+import com.db4o.Db4oEmbedded;
+import com.db4o.ObjectContainer;
+import com.db4o.ObjectSet;
+import com.db4o.query.Query;
+
 /**
  *
  * @author Yleve
  */
 public class BorrarDB4o {
-    
+    public static void main(String[] args) {
+         //PATH base de datos
+        String pathBD = "C:\\Users\\Yleve\\Desktop\\BDOO\\gestionLaboral1819.yap";
+        
+        //Establecemos conexion con la BD
+        ObjectContainer db = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(),pathBD);
+        //Preparamos la consulta
+        Query consulta = db.query();
+        //Establecemos la consulta a traves del campo salario, indicamos que deben ganar menos de 1500
+        consulta.descend("salario").constrain(1500).smaller();
+        //Guardamos el resultado de la consulta en una lista
+        ObjectSet resultado = consulta.execute();
+        
+        if(resultado.size() == 0){
+            System.out.println("No existen empleados con los datos especificados.");
+        }else{
+            //Recorremos y borramos el empleado que cumple las condiciones
+            while(resultado.hasNext()){
+                Empleado emp_actual = (Empleado)resultado.next();
+                db.delete(emp_actual);
+            }
+        }
+        //Cerramos
+        db.close();
+    }
 }
