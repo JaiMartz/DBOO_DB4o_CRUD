cfef3ba27e2bfbd6ed446d03090495ce8f38e000
JaiMartz <martinezgarridojairo@gmail.com>
2019-03-22 19:17:08 +0000

    We've create a little program which inject the employees in ours Data Base DB4o

--- src/ad_db4o/CrearDB4o.java
+++ src/ad_db4o/CrearDB4o.java
@@ -5,6 +5,8 @@
  */
 package ad_db4o;
 
+import com.db4o.*;
+
 /**
  *
  * @author Yleve
@@ -16,6 +18,28 @@ public class CrearDB4o {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        String urlDB = "C:\\Users\\Yleve\\Desktop\\BDOO\\gestionLaboral1819.yap";
+        
+        //Creamos la conexion con la BD
+        ObjectContainer db = Db4oEmbedded.openFile(Db4oEmbedded.newConfiguration(), urlDB);
+        
+        //Creamso los objetos que vamos a introducir en la BD
+        Empleado emp1 = new Empleado(1,"Juan","Martín","Administrativo",1200);
+        Empleado emp2 = new Empleado(2,"Ana","Amezcua","Programador Senior",1500);
+        Empleado emp3 = new Empleado(3,"Raúl","Perez","Programador Junior",1400);
+        Empleado emp4 = new Empleado(4,"Jairo","Martínez","Programador Junior",1400);
+        Empleado emp5 = new Empleado(5,"Celia","Carrillo","Jefe de equipo",1800);
+        
+        //Almacenamos los empleados en la BD
+        db.store(emp1);
+        db.store(emp2);
+        db.store(emp3);
+        db.store(emp4);
+        db.store(emp5);
+        
+        //Cerramos conexion
+        db.close();
     }
     
 }
